7d878266fd8b3511eb1fbd2e17c9b2013def4e2e
Prince Jha <princejha1456@gmail.com>
2026-04-25 23:40:15 +0530

    Add initial OLED display setup and message

---
+++ ArduinoUno_0.96Oled/ArduinoUno_0.96Oled.ino
@@ -0,0 +1,22 @@
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET    -1        // no reset pin
+#define SCREEN_ADDRESS 0x3C    // try 0x3D if this doesn't work
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+
+void setup() {
+  display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS);
+  display.clearDisplay();
+  display.setTextSize(1);
+  display.setTextColor(SSD1306_WHITE);
+  display.setCursor(0, 0);
+  display.println("Hello Prince!");
+  display.display();
+}
+
+void loop() {}
